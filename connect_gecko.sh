84396d703620d38eac85f289371f42907a21d8ab
Fredrik Hederstierna <fredrik.hederstierna@verisure.com>
2017-05-12 08:48:02 +0200

    added olimex

--- connect_gecko.sh
+++ connect_gecko.sh
@@ -5,3 +5,6 @@ sudo ./oo4all -f board/efm32.cfg
 
 # use external dongle ST-Link-v2
 #sudo ./oo4all -f connect_gecko_stlink.cfg
+
+# use external dongle Olimex
+#sudo ./oo4all -f connect_gecko_olimex-h.cfg
