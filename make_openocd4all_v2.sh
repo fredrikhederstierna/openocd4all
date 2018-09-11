7a4c254b6ccd40b545fe4c2ad9d3e560c9969f2d
Franz Levin <frazz@kryo.se>
2018-09-11 12:27:07 +0200

    Added missing dependencies for Ubuntu 18.04

--- make_openocd4all_v2.sh
+++ make_openocd4all_v2.sh
@@ -31,6 +31,10 @@
 #   sudo apt-get install libftdi1-dbg
 #   sudo apt-get install libftdi1-2
 #   sudo apt-get install libftdi1-2-dbg
+#   sudo apt-get install build-essential
+#   sudo apt-get install libtool
+#   sudo apt-get install automake
+#   sudo apt-get install pkg-config
 #
 # Note for CMSIS-DAP you nedd libhidapi-dev
 #   sudo apt-get install libhidapi-dev
